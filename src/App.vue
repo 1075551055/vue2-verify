66f08176af12b6029cb5d7092b68d60ee2ed2427
吴贻韡 <yiwei.wu@ipiaoniu.com>
2019-01-16 22:52:49 +0800

    lint: 格式化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <components></components>
+  <components
+    :is="type"
+    v-bind="$attrs"
+  ></components>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from "vue-property-decorator";
+import Slide from "./components/Slide.vue";
 
 @Component({
   components: {
-  },
+    Slide
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  @Prop({ default: "slide" }) public type!: string;
+}
 </script>
---
+++ src/components/Slide.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="position: relative">
+    <slot name="poppups"></slot>
+    <!-- 滑动条 -->
+    <div class="verify-bar-area"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue {
+  @Prop({ default: "" }) public myAtty!: string;
+}
+</script>
+<style lang="stylus">
+.verify-bar-area {
+  position: relative;
+  background: #FFFFFF;
+  text-align: center;
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  border: 1px solid #ddd;
+  -webkit-border-radius: 4px;
+}
+</style>
+
